125ac620dfabbdce3cd7d70388af016a3ea84c4b
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel-370.local>
2019-04-09 01:19:29 -0300

    calculating cost example

--- simple_cvrp.py
+++ simple_cvrp.py
@@ -1,7 +1,10 @@
 # COMO USAR python simple_cvrp.py --file "CVRP/test.txt"
 
-import random,sys,copy
+import random,sys,copy,math
 from optparse import OptionParser
+
+#          truck,node1,node2,node3,node4
+solution = [[1,1,2,3,4,5,6,7,8,9,10]]
 
 class cvrp:
     def __init__(self, filename):
@@ -11,7 +14,11 @@ class cvrp:
         self.capacity = 0
         self.deposit = None
         self.init_data(self.filename)
-        self.cost = self.calc_cost()
+
+        # test one cost
+        (xa, ya) = self.retrieve_data(2)
+        (xb, yb) = self.retrieve_data(1)
+        self.cost = self.calc_cost(int(xa), int(xb), int(ya), int(yb))
 
     def init_data(self,filename):
         node_coord = False
@@ -47,6 +54,9 @@ class cvrp:
         self.deposit = self.nodes[0]
         self.nodes.pop(0)
 
+    def retrieve_data(self,index):
+        return self.nodes[int(index)][1],self.nodes[int(index)][2]
+
     def printStatus(self):
         print("\n")
         print("======")
@@ -55,15 +65,32 @@ class cvrp:
         print("Quantidade Depositos = ", self.nodesCount)
         print("Capacidade = ", self.capacity)
         print("Deposito = ", self.deposit)
-        print("Nós:")
-        for i in self.nodes:
-            print(i)
+        print("Custo = ", self.cost)
+        # print("Nós:")
+        # for i in self.nodes:
+        #     print(i)
 
     def hill_climbing():
         return 0
 
-    def calc_cost(self):
-        return 0
+    def calc_cost(self, xa, xb, ya, yb):
+        return math.sqrt(((xa-xb) * (xa-xb))+((ya-yb) * (ya-yb)))
+
+    def check_quantity(self):
+        qtyOk = 0
+        count = 0
+        truckQty = int(self.capacity)
+        for trip in solution:
+            node_length = len(trip)-1
+            for node in range(0, node_length):
+                print(int(self.nodes[int(node)][3]))
+                truckQty = truckQty - int(self.nodes[int(node)][3])
+                if truckQty < 0:
+                    qtyOk = qtyOk -1
+                print("current quantity", truckQty)
+        if qtyOk == 0:
+            qtyOk = qtyOk+1
+        return qtyOk
 
 if __name__ == "__main__":
 
